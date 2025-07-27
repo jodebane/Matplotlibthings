cb7f80b997f189712b8afaf5de0bfc620b272058
Joseph Foley <josephfoley@rcn.com>
2025-07-27 20:34:15 +0300

    Update airportrank.py
    reads from a csv file with the 50 busiest airports of 2024 (ranked by passenger traffic)

--- airportrank.py
+++ airportrank.py
@@ -1,20 +1,51 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import csv
+
 
 
 
 #info
 
-airports  = ['ATL', 'DXB', 'DFW', 'HND', 'LHR', 'DEN', 'ORD', 'IST', 'DEL', 'PVG']
-passengernum = [108, 92, 88, 85, 84, 82, 80, 80, 78, 77]
+airports  = []
+passengernum = []
+
+##reads from the csv file
+
+
+import csv
+with open('airports.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for lines in csvFile:
+        airports.append(lines[0])
+        strvar = (lines[1].replace(',', ''))
+        numvar = int(strvar)
+        passengernum.append(numvar)
+        print(lines)
+
+print(airports)
+print(passengernum)
+
+##create dictionary
+
+font = {'size': 10}
+
+##font for plot 
+
+plt.rc('font', **font)
 
 
 #plotting graph
 plt.bar(airports, passengernum)
 plt.title('Busiest airports of 2024')
-plt.xlabel('Airport code')
-plt.ylabel('Passenger Numbers in millions (rounded to nearest)')
+plt.xlabel('Airport name')
+plt.ylabel('Passenger Numbers')
+
+##initialize y limit
+
+plt.ylim(0,150000000)
 
 #display graph
+
 plt.show()
